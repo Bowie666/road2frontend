95393dd5fafb0f3b4fc9cb45d8595d5879bd2cf7
xuwei <ldldxw@163.com>
2021-03-29 15:48:55 +0800

    基于 axios 封装的请求模块

---
+++ src/utils/requests.js
@@ -0,0 +1,19 @@
+// 基于 axios 封装的请求模块 npm i axios
+import 'axios'
+
+// 创建一个 axios 实例，说白了就是复制了一个 axios
+// 我们通过这个实例去发请求，把需要的配置配置给这个实例来处理
+const request = axios.create({
+    baseURL: 'http://www.xxx.xxx/' // 请求的基础路径
+  })
+
+// 导出请求方法
+export default request
+
+// 谁要使用谁就加载 request 模块
+// import request from 'request.js'
+// request.xxx
+// request({
+//   method: ,
+//   url: ''
+// })
