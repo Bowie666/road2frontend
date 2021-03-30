d083b1c5b1e3a3de65268d5b6157614b29dfe8a7
xuwei <ldldxw@163.com>
2021-03-30 08:49:22 +0800

    登陆组件编写

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,19 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
+// 如果文件里面的文件就叫 index.vue 就可以省略不写
+import Login from '@/views/login/index.vue'
+
 Vue.use(VueRouter)
 
 // 路由配置表
-const routes = []
+const routes = [
+  {
+    path: '/login',
+    name: 'login',
+    component: Login
+  }
+]
 
 const router = new VueRouter({
   routes
---
+++ src/views/login/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class='login-container'>登陆页面</div>
+</template>
+
+<script>
+export default {
+  name: 'LoginIndex',
+  components: {},
+  props: {},
+  data () {
+    return {}
+  },
+
+  watch: {},
+  computed: {},
+
+  created: {},
+  mounted: {},
+  methods: {}
+}
+</script>
+
+<style scoped lang='less'>
+</style>
